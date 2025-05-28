8e98c67fef55993c2db03a2d4b61894dddf0f714
martksk <krtsk2547@gmail.com>
2025-05-28 21:25:56 +0700

    test cd3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -106,7 +106,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Como Laundry 2</title>
+        <title>Como Laundry 3</title>
       </Head>
       <div className="w-full">
         <div className="w-full mb-20">
